5f30e7e7a677b77192015976e7a2db9677a4909c
ericokeefe <okeefee@purdue.edu>
2023-04-24 12:28:07 -0400

    added front, left, back, and right distances using laserscan. outlined state machine.

--- src/final_project/src/task_1.py
+++ src/final_project/src/task_1.py
@@ -26,11 +26,8 @@ class Explorer:
         self.rate = rospy.Rate(30)
 
         # Subscribers
-        # TODO: might not need this
-        # rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.__goal_pose_cbk, queue_size=1)
-
         rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.__ttbot_pose_cbk, queue_size=1)
-        #rospy.Subscriber('/scan', LaserScan, self.__laser_scan_cbk, queue_size=1)
+        rospy.Subscriber('/scan', LaserScan, self.__laser_scan_cbk, queue_size=1)
 
         # Publishers
         self.path_pub = rospy.Publisher('global_plan', Path, queue_size=1)
@@ -38,17 +35,6 @@ class Explorer:
         self.show_path_pub = rospy.Publisher('astar_plan', Path, queue_size=1)
 
         rospy.loginfo('Node Initialized!')
-
-    # TODO: Might not need this
-    """
-    def __goal_pose_cbk(self, data):
-        # Callback to catch the goal pose.
-        #@param  data    PoseStamped object from RVIZ.
-        #@return None.
-    
-        self.goal_pose = data
-        rospy.loginfo('goal_pose:{:.4f},{:.4f}'.format(self.goal_pose.pose.position.x, self.goal_pose.pose.position.y))
-    """
 
     def __ttbot_pose_cbk(self, data):
         """! Callback to catch the position of the vehicle.
@@ -66,54 +52,77 @@ class Explorer:
                 break
 
     def __laser_scan_cbk(self, data):
-        self.laser_min = data.range_min
-        self.laser_max = data.range_min
+        # self.laser_min = data.range_min
+        # self.laser_max = data.range_max
         self.laser_ranges = data.ranges
+        '''
+        rospy.loginfo('0: {:.4f}, 89: {:.4f}, 179: {:.4f}, 269; {:.4f}'.format(self.laser_ranges[0],
+                                                                               self.laser_ranges[89],
+                                                                               self.laser_ranges[179],
+                                                                               self.laser_ranges[269]))
+        '''
 
-    def move_ttbot(self, speed, heading):
-        """ Function to move turtlebot passing directly a heading angle and the speed.
-        @param  speed     Desired yaw angle.
-        @param  heading   Desired speed.
-        @return path      object containing the sequence of waypoints of the created path.
+        # find front, left, back, right distances
+        self.fov = 2  # (total fov / 2) - 1
+        self.front_dist = min(min(self.laser_ranges[0:(0 + self.fov)], self.laser_ranges[(359 - (self.fov - 1)):359]))
+        self.left_dist = min(self.laser_ranges[(89 - self.fov):(89 + self.fov)])
+        self.back_dist = min(self.laser_ranges[(179 - self.fov):(179 + self.fov)])
+        self.right_dist = min(self.laser_ranges[(269 - self.fov):(269 + self.fov)])
+
+        rospy.loginfo('front: {:.2f}, left: {:.2f}, back: {:.2f}, right: {:.2f}'.format(self.front_dist,
+                                                                                        self.left_dist,
+                                                                                        self.back_dist,
+                                                                                        self.right_dist))
+
+        # find minimum distance and corresponding index
+        self.laser_range_min = 100
+        self.laser_range_min_index = None
+
+        for (i, item) in enumerate(self.laser_ranges):
+            if item < self.laser_range_min:
+                self.laser_range_min = item
+                self.laser_range_min_index = i
+
+        rospy.loginfo('range_min = {:.4f}, min_index: {:d}'.format(self.laser_range_min, self.laser_range_min_index))
+
+    def move_ttbot(self, speed, steer_angle):
+        """! Function to move turtlebot passing directly a speed and steering angle.
+        @param  speed     Desired speed.
+        @param  steer_angle   Desired steering angle.
         """
         cmd_vel = Twist()
 
-        # calculate current turtle bot orientation(yaw)
-        bot_pose = self.ttbot_pose
-
-        bot_quat = (bot_pose.pose.orientation.x, bot_pose.pose.orientation.y, bot_pose.pose.orientation.z,
-                    bot_pose.pose.orientation.w)
-        bot_euler = tf.transformations.euler_from_quaternion(bot_quat)
-        bot_yaw = bot_euler[2]
-
-        # calculate steering angle
-        # steering_angle = 2 * (heading - bot_yaw)
-
         cmd_vel.linear.x = speed
-        # cmd_vel.angular.z = steering_angle
-
-        # TODO: using steering angle instead of heading but still named heading
-        cmd_vel.angular.z = heading
+        cmd_vel.angular.z = steer_angle
 
         self.cmd_vel_pub.publish(cmd_vel)
 
-    def follow_wall(self):
-        speed = 0
-        heading = 0
+    def find_wall(self):
+        # state 0 in state machine
+        # rotates right until self.front_dist is minimized
 
-        return speed, heading
+        return
+
+    def approach_wall(self):
+        # state 1 in state machine
+        # drives straight until self.front_dist hits desired distance
+
+        return
+
+    def follow_wall(self):
+        # state 2 in state machine
+        # turns left until self.right_dist is minimized
+        # drives straight with left/right corrections to follow wall on right side of vehicle
+
+        return
 
     def run(self):
         """ Main loop of node to run
         """
         while not rospy.is_shutdown():
-            rospy.loginfo('Explorer node alive ...')
+            #rospy.loginfo('Explorer node alive ...')
 
-            #rospy.loginfo('laser min, max:{:.4f}'.format(self.laser_min, self.laser_max))
-
-            #speed, heading = self.follow_wall()
-
-            #self.move_ttbot(speed, heading)
+            # TODO: implement state machine
 
             self.rate.sleep()
         # rospy.signal_shutdown("[{}] Finished Cleanly".format(self.name))
